0797e4519c897724dbcdae093dee80b2b8baed4d
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2025-12-14 14:00:11 +0800

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -7,5 +7,8 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
 });
