ce9c742c0269a771c4c2717d19781bb460c1a69b
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2025-12-14 17:59:37 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,13 +10,15 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://predix-five.vercel.app"
+      "https://predix-five.vercel.app",
     ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 
